a7c43fce7689faa8e4c94b78601b53b9253ecdb2
Suzan-Queiroz <102221677+Suzan-Queiroz@users.noreply.github.com>
2022-04-26 01:47:14 -0300

    Add files via upload

---
+++ Programa.cs
@@ -0,0 +1,33 @@
+using System;
+
+	public class Program
+	{
+		public static void Main()
+		{
+			Quadrado q1 = new Quadrado(10.0);
+            Quadrado q2 = new Quadrado(4.5);
+
+            q1.Lado = 15;
+
+            Console.WriteLine("\n--- Quadrado 1 ---\n");
+            Console.WriteLine("Lado: " +q1.Lado);
+            Console.WriteLine("Area: " +q1.Area);
+            Console.WriteLine("Perimetro: " +q1.Perimetro);
+
+            Console.WriteLine("\n--- Quadrado 2 ---\n");
+            Console.WriteLine("Lado: " +q2.Lado);
+            Console.WriteLine("Area: " +q2.Area);
+            Console.WriteLine("Perimetro: " +q2.Perimetro);
+
+            Retangulo r1 = new Retangulo(10, 4);
+            Retangulo r2 = new Retangulo(20, 8);
+
+            Console.WriteLine("\n--- Retangulo 1 ---\n");
+            Console.WriteLine("Area: " +r1.Area);
+            Console.WriteLine("Perimetro: " +r1.Perimetro);
+
+            Console.WriteLine("\n--- Retangulo 2 ---\n");
+            Console.WriteLine("Area: "+r2.Area);
+            Console.WriteLine("Perimetro: " +r2.Perimetro);
+		}
+	}
