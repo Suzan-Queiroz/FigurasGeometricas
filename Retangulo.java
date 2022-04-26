57f27ba098357028b5c2a0fd17af7b8bdf9ccdb5
Suzan-Queiroz <102221677+Suzan-Queiroz@users.noreply.github.com>
2022-04-26 00:59:06 -0300

    Conclude the task

---
+++ Retangulo.java
@@ -0,0 +1,16 @@
+public class Retangulo
+{
+    public double Base{ get; set; }
+    public double Altura{get; set;}
+    public double Area { get => Base * Altura;}
+    
+    public double Perimetro { 
+    get => (2*Base+2*Altura);
+    }
+
+    public Retangulo(double b, double altura)
+    {
+        Altura = altura;
+        Base = b;
+    }
+}
